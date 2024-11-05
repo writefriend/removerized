ebbda80e96391528f64aae81e5fc54567723aeff
writefriend <154815311+writefriend@users.noreply.github.com>
2024-11-05 22:26:08 +0800

    Update footer.tsx
    update something

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,21 +1,7 @@
 export const Footer = () => {
   return (
     <span className="border-t p-4 text-center text-xs text-muted-foreground md:mx-0 md:p-6 md:text-start">
-      Built by{" "}
-      <a
-        className="text-foreground hover:underline"
-        target="_blank"
-        href="https://yossthedev.github.io"
-      >
-        @yossthedev
-      </a>
-      . The source code is available on{" "}
-      <a
-        className="text-foreground hover:underline"
-        target="_blank"
-        href="https://github.com/yossTheDev/removerized"
-      >
-        GitHub
+      Built by{" 字友 "}
       </a>
     </span>
   )
