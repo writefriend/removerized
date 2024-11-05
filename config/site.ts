b83e38e6fa88bfb31748660ddffb93d2f6df84cb
writefriend <154815311+writefriend@users.noreply.github.com>
2024-11-05 22:17:55 +0800

    Update site.ts
    去掉一些链接

--- config/site.ts
+++ config/site.ts
@@ -5,26 +5,5 @@ export const siteConfig = {
 
   description:
     "Effortlessly Remove Image Backgrounds with AI - Free & Limitless with Offline Support",
-  mainNav: [
-    {
-      title: "License",
-      href: "https://github.com/yossTheDev/boostedpic/blob/main/LICENSE",
-      external: true
-    },
-    {
-      title: "Donation",
-      href: "https://qvapay.com/payme/yoannisgnw",
-      external: true
-    },
-    {
-      title: "Send Feedback",
-      href: "https://tally.so/r/nrlKdM",
-      external: true,
-    },
-  ],
-  links: {
-    twitter: "https://twitter.com/yossthedev",
-    github: "https://github.com/yossTheDev/removerized",
-    telegram: "https://t.me/yosstdv"
-  },
+  mainNav: []
 }
